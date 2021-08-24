ac3c4290b25d4301bb9fb14c4a36b5de0992e9f7
https://apapishev@bitbucket.org <apapishev@mineradio.ru>
2021-08-24 13:54:27 +0700

    test next lesson

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,8 @@ from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = MainPage(browser, link)
     page.open()
     product_page = ProductPage(browser, browser.current_url)
